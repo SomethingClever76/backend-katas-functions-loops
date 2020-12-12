29a387e2598936f16ac91388466a20a98aadaae2
Kyle <mrkylethomas76@gmail.com>
2020-12-12 18:57:21 -0500

    Complete multiply function

--- main.py
+++ main.py
@@ -9,13 +9,43 @@ __author__ = "Kyle Thomas"
 def add(x, y):
     """Add two integers."""
     # your code here
+    # print(x + y)
     return x + y
+
+
+# add(2, 4)
 
 
 def multiply(x, y):
     """Multiply x with y using the add() function above."""
     # your code here
-    return
+    # total acts as an accumulator
+    total = 0
+
+    # for x, y - find the higher abs() value:
+    # then use the add() function abs(higher number) of times
+    # while subtracting abs(higher number) each time
+    if abs(x) > abs(y):
+        for i in range(0, abs(x)):
+            total -= add(abs(x), abs(y)) - abs(x)
+    else:
+        for i in range(0, abs(y)):
+            total -= add(abs(x), abs(y)) - abs(y)
+
+    # determine whether total returned should be positive or negative:
+    # if one of the two numbers is negative:
+    #   the total will be negative
+    # when both numbers are negative or both numbers are positive:
+    #   the total is always positive
+    if x < 0 and y < 0 or x > 0 and y > 0:
+        print(abs(total))
+        return abs(total)
+    else:
+        print(total)
+        return total
+
+
+# multiply(6, -8)
 
 
 def power(x, n):
